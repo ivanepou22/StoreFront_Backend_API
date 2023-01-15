432b688c4861510098b5178a2fc4b0e341750240
Ivan Epou <ivanepou92@gmail.com>
2023-01-15 22:38:05 +0300

    Add dotenv package and use env variables

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json())
 
@@ -10,6 +13,6 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log(`starting app on: ${address}`)
 })
